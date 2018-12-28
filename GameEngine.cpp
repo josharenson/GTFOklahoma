3b2a910a156696b5739ae489aae389095bd03c63
josharenson <josharenson@gmail.com>
2018-12-28 11:10:02 -0800

    fixup!

--- GameEngine.cpp
+++ GameEngine.cpp
@@ -28,13 +28,12 @@ GameEngine::~GameEngine()
 
 void GameEngine::addItemToInventory(const QString &itemName)
 {
-   // if(m_playerInventoryModel->addItemToInventory(itemName)) {
-    m_playerInventoryModel->addItemToInventory(itemName); 
+   if(m_playerInventoryModel->addItemToInventory(itemName)) {
         QHash<QString, qreal> itemStats = m_playerStatsModel->statsForItem(itemName);
         foreach (const auto &key, itemStats.keys()) {
             m_playerStatsModel->updateStat(key, itemStats[key]);
         }
-   // }
+   }
 }
 
 QString GameEngine::currentPlayer() const
